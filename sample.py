a0bf78939db01f3c30c4c25b192a9053576ef815
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 16:04:24 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   Scanner sc=new Scanner(System.in);
+   int num=sc.nextInt();
+   int power=sc.nextInt();
+   int result=1;
+   while(power!=0)
+   {
+   	result*=num;
+   	power--;
+   }
+   System.out.println(result);
+   	
+   }
+ }
+}
